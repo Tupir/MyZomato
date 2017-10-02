7907ecbd359481d54dc35adfadb56798f7085b2d
Tupir <gaborpeter11@gmail.com>
2017-10-03 00:41:15 +0200

    added header

--- app/src/main/java/com/example/android/myzomato/MainActivity.java
+++ app/src/main/java/com/example/android/myzomato/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.android.myzomato;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.myzomato.Utils.NetworkUtils;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>{
+    private static final int FORECAST_LOADER_ID = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportLoaderManager().initLoader(FORECAST_LOADER_ID, null, this).forceLoad();
+
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return new AsyncTaskLoader<String>(this) {
+
+            String recepts = null;
+
+            @Override
+            protected void onStartLoading() {
+                System.out.println("zacalo");
+            }
+
+
+            @Override
+            public String loadInBackground() {
+
+
+                URL receptRequestUrl = NetworkUtils.buildUrl();
+
+                try {
+                    String jsonWeatherResponse = NetworkUtils
+                            .getResponseFromHttpUrl(receptRequestUrl);
+
+
+                    System.out.println(jsonWeatherResponse);
+                    return jsonWeatherResponse;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
     }
 }
---
+++ app/src/main/java/com/example/android/myzomato/Utils/NetworkUtils.java
@@ -0,0 +1,75 @@
+package com.example.android.myzomato.Utils;
+
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Create network connection and retrieve data
+ *
+ */
+
+public final class NetworkUtils {
+
+    private NetworkUtils() {}
+
+    // url
+    final static String RECEPT_URL = "https://developers.zomato.com/api/v2.1/categories";
+
+    /**
+     * Builds the URL.
+     * Vysklada URL adresu
+     */
+    public static URL buildUrl() {
+
+
+        Uri builtUri = Uri.parse(RECEPT_URL).buildUpon()
+                .
+                build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(url.toString());
+        return url;
+    }
+
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     * Vrati response zo stranky (cely JSON)
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        String user_key = "ce3ebff8fc6db1d6714583670a7d4434";
+        urlConnection.setRequestProperty("user-key", user_key); // added header
+        try {
+            urlConnection.setConnectTimeout(5000);  // if there are some connection problems hold on 5 seconds
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+
+}
