591d0e7e3d8f78af350b80c9d5cd8685aa9722ed
Tupir <gaborpeter11@gmail.com>
2017-10-18 00:26:06 +0200

    task 2 finish

--- app/src/main/java/com/example/android/myzomato/MainActivity.java
+++ app/src/main/java/com/example/android/myzomato/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.android.myzomato;
 
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
-import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,14 +13,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 
-import com.example.android.myzomato.Utils.NetworkUtils;
 import com.example.android.myzomato.data.RestaurantDbHelper;
 import com.example.android.myzomato.data.RestaurantTableContents;
 import com.example.android.myzomato.sync.ZomatoSyncUtils;
 
-import java.net.URL;
-
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>,
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>,
         RestaurantAdapter.ForecastAdapterOnClickHandler {
 
 
@@ -36,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
 
 
-    private static final int FORECAST_LOADER_ID = 0;
+    private static final int LOADER_ID = 0;
     private RestaurantAdapter restaurantAdapter;
     private RecyclerView mRecyclerView;
     private int mPosition = RecyclerView.NO_POSITION;
@@ -52,6 +49,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_forecast);
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+        showLoading();
+
         LinearLayoutManager layoutManager =
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(layoutManager);
@@ -60,22 +60,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         restaurantAdapter = new RestaurantAdapter(this, this);
         mRecyclerView.setAdapter(restaurantAdapter);
 
-
-
         RestaurantDbHelper dbHelper = RestaurantDbHelper.getInstance(this);
         mDb = dbHelper.getWritableDatabase();
 
-
-        getSupportLoaderManager().initLoader(FORECAST_LOADER_ID, null, this);
+        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
         ZomatoSyncUtils.initialize(this);
 
     }
 
     private void showLoading() {
-        /* Then, hide the weather data */
         mRecyclerView.setVisibility(View.INVISIBLE);
-        /* Finally, show the loading indicator */
         mLoadingIndicator.setVisibility(View.VISIBLE);
+    }
+
+    private void showData() {
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
     }
 
     private Cursor getAllFavoriteMovies() {
@@ -91,91 +91,49 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     @Override
-    public Loader<String> onCreateLoader(int id, Bundle args) {
-        return new AsyncTaskLoader<String>(this) {
-
-            String recepts = null;
-            String jsonRestaurantResponse;
-            @Override
-            protected void onStartLoading() {
-                showLoading();
-                System.out.println("zacalo");
-            }
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 
 
-            @Override
-            public String loadInBackground() {
+        switch (id) {
 
+            case LOADER_ID:
+                /* URI for all rows of all data in our weather table */
+                Uri forecastQueryUri = RestaurantTableContents.RestaurantEntry.CONTENT_URI;
+                //String sortOrder = "ROWID LIMIT 5";
+                String sortOrder = "RANDOM() LIMIT 5";
 
-                URL receptRequestUrl = NetworkUtils.buildUrl("https://developers.zomato.com/api/v2.1/search?entity_id=219&entity_type=city");
-                URL receptRequestUrl2 = NetworkUtils.buildUrl("https://developers.zomato.com/api/v2.1/search?entity_id=219&entity_type=city&start=20");
+                return new CursorLoader(this,
+                        forecastQueryUri,
+                        MAIN_RESTAURANT_PROJECTION,
+                        null,
+                        null,
+                        sortOrder);
 
-//                try {
-//                    jsonRestaurantResponse = NetworkUtils
-//                            .getResponseFromHttpUrl(receptRequestUrl);
-//
-//                    String jsonRestaurantResponse2 = NetworkUtils
-//                            .getResponseFromHttpUrl(receptRequestUrl2);
-//
-//                    ContentValues[] vals = RestaurantJsonParser.getRestaurantDataFromJson(jsonRestaurantResponse);
-//                    int jop = vals.length;
-//                    ContentValues[] vals2 = RestaurantJsonParser.getRestaurantDataFromJson(jsonRestaurantResponse2);
-//                    jop = vals.length;
-//                    ContentValues[] array1and2 = concatArrays(vals, vals2);
-//
-//
-//                    if (vals != null && vals.length != 0) {
-//                        ContentResolver sunshineContentResolver = getContext().getContentResolver();
-//
-//                        sunshineContentResolver.delete(
-//                                RestaurantTableContents.RestaurantEntry.CONTENT_URI,
-//                                null,
-//                                null);
-//
-//                        sunshineContentResolver.bulkInsert(
-//                                RestaurantTableContents.RestaurantEntry.CONTENT_URI,
-//                                array1and2);
-//                    }
-//
-//
-//
-//                    System.out.println(jsonRestaurantResponse);
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                    return null;
-//                }
-
-                return jsonRestaurantResponse;
-            }
-
-
-        };
-    }
-
-    @Override
-    public void onLoadFinished(Loader<String> loader, String data) {
-
-        Cursor cursor = getAllFavoriteMovies();
-        int ops = cursor.getCount();
-        mLoadingIndicator.setVisibility(View.INVISIBLE);
-    }
-
-    public ContentValues[] concatArrays(ContentValues[] one, ContentValues[] two){
-        ContentValues[] array1and2 = new ContentValues[one.length + two.length];
-        System.arraycopy(one, 0, array1and2, 0, one.length);
-        System.arraycopy(two, 0, array1and2, one.length, two.length);
-        return array1and2;
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + id);
+        }
 
     }
 
     @Override
-    public void onLoaderReset(Loader<String> loader) {
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 
+        restaurantAdapter.swapCursor(data);
+        if (mPosition == RecyclerView.NO_POSITION) mPosition = 0;
+        mRecyclerView.smoothScrollToPosition(mPosition);
+        int i = data.getCount();
+        if(data.getCount() != 0)
+            showData();
+    }
+
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        restaurantAdapter.swapCursor(null);
     }
 
     @Override
-    public void onClick(long date) {
+    public void onClick() {
 
     }
 }
--- app/src/main/java/com/example/android/myzomato/RestaurantAdapter.java
+++ app/src/main/java/com/example/android/myzomato/RestaurantAdapter.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by PepovPC on 10/15/2017.
@@ -16,30 +18,18 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Fo
 
     private final Context mContext;
 
-    /*
-     * Below, we've defined an interface to handle clicks on items within this Adapter. In the
-     * constructor of our ForecastAdapter, we receive an instance of a class that has implemented
-     * said interface. We store that instance in this variable to call the onClick method whenever
-     * an item is clicked in the list.
-     */
+
     final private ForecastAdapterOnClickHandler mClickHandler;
 
-    /**
-     * The interface that receives onClick messages.
-     */
+    // I send data which I need in following activity
     public interface ForecastAdapterOnClickHandler {
-        void onClick(long date);
+        void onClick();
     }
 
     private Cursor mCursor;
 
-    /**
-     * Creates a ForecastAdapter.
-     *
-     * @param context Used to talk to the UI and app resources
-     * @param clickHandler The on-click handler for this adapter. This single handler is called
-     *                     when an item is clicked.
-     */
+
+
     public RestaurantAdapter(@NonNull Context context, ForecastAdapterOnClickHandler clickHandler) {
         mContext = context;
         mClickHandler = clickHandler;
@@ -59,14 +49,13 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Fo
     @Override
     public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
 
-//        View view = LayoutInflater
-//                .from(mContext)
-//                .inflate(R.layout.forecast_list_item, viewGroup, false);
-//
-//        view.setFocusable(true);
-//
-//        return new ForecastAdapterViewHolder(view);
-        return null;
+        View view = LayoutInflater
+                .from(mContext)
+                .inflate(R.layout.restaurant_item, viewGroup, false);
+
+        view.setFocusable(true);
+
+        return new ForecastAdapterViewHolder(view);
     }
 
     /**
@@ -84,49 +73,19 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Fo
         mCursor.moveToPosition(position);
 
 
-//        /*******************
-//         * Weather Summary *
-//         *******************/
-//        /* Read date from the cursor */
-//        long dateInMillis = mCursor.getLong(MainActivity.INDEX_WEATHER_DATE);
-//        /* Get human readable string using our utility method */
-//        String dateString = SunshineDateUtils.getFriendlyDateString(mContext, dateInMillis, false);
-//        /* Use the weatherId to obtain the proper description */
-//        int weatherId = mCursor.getInt(MainActivity.INDEX_WEATHER_CONDITION_ID);
-//        String description = SunshineWeatherUtils.getStringForWeatherCondition(mContext, weatherId);
-//        /* Read high temperature from the cursor (in degrees celsius) */
-//        double highInCelsius = mCursor.getDouble(MainActivity.INDEX_WEATHER_MAX_TEMP);
-//        /* Read low temperature from the cursor (in degrees celsius) */
-//        double lowInCelsius = mCursor.getDouble(MainActivity.INDEX_WEATHER_MIN_TEMP);
-//
-//        String highAndLowTemperature =
-//                SunshineWeatherUtils.formatHighLows(mContext, highInCelsius, lowInCelsius);
-//
-//        String weatherSummary = dateString + " - " + description + " - " + highAndLowTemperature;
-//
-//        forecastAdapterViewHolder.weatherSummary.setText(weatherSummary);
+        String name = mCursor.getString(MainActivity.INDEX_COLUMN_NAME);
+
+        forecastAdapterViewHolder.restaurantSummary.setText(name);
     }
 
-    /**
-     * This method simply returns the number of items to display. It is used behind the scenes
-     * to help layout our Views and for animations.
-     *
-     * @return The number of items available in our forecast
-     */
+
     @Override
     public int getItemCount() {
         if (null == mCursor) return 0;
         return mCursor.getCount();
     }
 
-    /**
-     * Swaps the cursor used by the ForecastAdapter for its weather data. This method is called by
-     * MainActivity after a load has finished, as well as when the Loader responsible for loading
-     * the weather data is reset. When this method is called, we assume we have a completely new
-     * set of data, so we call notifyDataSetChanged to tell the RecyclerView to update.
-     *
-     * @param newCursor the new cursor to use as ForecastAdapter's data source
-     */
+
     void swapCursor(Cursor newCursor) {
         mCursor = newCursor;
         notifyDataSetChanged();
@@ -138,12 +97,12 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Fo
      * OnClickListener, since it has access to the adapter and the views.
      */
     class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        //final TextView weatherSummary;
+        final TextView restaurantSummary;
 
         ForecastAdapterViewHolder(View view) {
             super(view);
 
-            //weatherSummary = (TextView) view.findViewById(R.id.tv_weather_data);
+            restaurantSummary = view.findViewById(R.id.restaurant_data);
 
             view.setOnClickListener(this);
         }
@@ -159,8 +118,7 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Fo
         public void onClick(View v) {
             int adapterPosition = getAdapterPosition();
             mCursor.moveToPosition(adapterPosition);
-//            long dateInMillis = mCursor.getLong(MainActivity.INDEX_WEATHER_DATE);
-//            mClickHandler.onClick(dateInMillis);
+            mClickHandler.onClick();
         }
     }
 
